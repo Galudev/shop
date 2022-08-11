700e400c6297c1ccdec7eee2120b4efe09afd151
Galudev <lucia.galudev@gmail.com>
2022-08-11 14:25:31 +0200

    Guardado de items en el carrito

--- src/pages/ShopItem.jsx
+++ src/pages/ShopItem.jsx
@@ -1,8 +1,19 @@
 import { IconButton } from "@mui/material"
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
-// TODO: Cambiar a Material UI
+// TODO: poner data con useState
+// TODO: guardar objeto con id y cantidad
+// TODO: Mostrar alerta añadir de nuevo
 export const ShopItem = ({ furniture }) => {
+
+    let data = [];
+
+    const addToBuy = (event) => {
+        data = JSON.parse(localStorage.getItem('buy')) ?? [];
+        data = [...data, furniture.id];
+        localStorage.setItem('buy', JSON.stringify(data));
+    }
+
     return (
         <div className="col-12 col-lg-4">
             <div className="card m-2">
@@ -11,10 +22,19 @@ export const ShopItem = ({ furniture }) => {
                     alt={furniture.name}
                     className="card-img-top"
                 />
-                <div className="card-body">
-                    <h3 className="card-title">{furniture.name}</h3>
-                    <h5 className="card-text float-end">{furniture.price}</h5>
-                    <IconButton className="btn btn-secondary"> <AddShoppingCartIcon /> </IconButton>
+                <div className="card-body row">
+                    <div className="col-10">
+                        <h3 className="card-title">{furniture.name}</h3>
+                        <h5 className="card-text">{furniture.price}</h5>
+                    </div>
+                    <div className="col-2">
+                        <IconButton
+                            className="btn btn-secondary position-absolute bottom-0 end-0 translate-middle"
+                            onClick={addToBuy}
+                        >
+                            <AddShoppingCartIcon />
+                        </IconButton>
+                    </div>
                 </div>
             </div>
         </div>
