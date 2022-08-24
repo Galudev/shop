ba298037a6a00636e51c472320a52b772b17a872
Galudev <lucia.galudev@gmail.com>
2022-08-24 13:32:03 +0200

    Implementado el funcionamiento de la página de contacto y validación del formulario

--- Readme.md
+++ Readme.md
@@ -3,7 +3,6 @@
 ** Este documento contará con las funcionalidades que quedan pendientes de implementar, así como futuros cambios sobre las funcionalidades o el diseño ya creado.
 
 ## Nuevas funcionalidades
-* Añadir funcionamiento de la página de contacto.
 * Añadir página de detalles de un elemento.
 * Implementar una página de autenticación para los usuarios.
 * Implementar formalización de la compra.
@@ -12,4 +11,4 @@
 * Modificar las opciones de filtro por categoría para automatizarlo.
 
 ## Nuevos proyectos
-* Realizar API para base de datos de los productos, usuarios y carrito.
+* Realizar API para base de datos de los productos, usuarios y carrito, y que envíe los mensajes de contacto al administrador.
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from '../hooks'
 
 const contactFormFields = {
@@ -7,13 +7,24 @@ const contactFormFields = {
     message: ''
 };
 
+const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+const formValidations = {
+    name: [(value) => value.length > 0, 'El nombre es obligatorio'],
+    email: [(value) => regexEmail.test(value), 'Introduzca un correo electrónico válido'],
+    message: [(value) => value.length > 0, 'Introduzca un mensaje']
+}
+
 export const Contact = () => {
 
-    const { name, email, message, onInputChange } = useForm(contactFormFields);
+    const { name, email, message, onInputChange, nameValid, emailValid, messageValid, isFormValid } = useForm(contactFormFields, formValidations);
+    const [formSubmitted, setFormSubmitted] = useState(false);
 
     const onSend = (event) => {
         event.preventDefault();
-
+        setFormSubmitted(true);
+        if (!isFormValid) return;
+        localStorage.setItem("contact", JSON.stringify({ name, email, message }));
     }
 
     return (
@@ -28,35 +39,44 @@ export const Contact = () => {
                             <label htmlFor="floatingInput">Nombre</label>
                             <input
                                 type="text"
-                                className="form-control form-field-contact"
+                                className={`form-control form-field-contact ${(!!nameValid && formSubmitted) ? 'is-invalid' : ''}`}
                                 placeholder="Nombre"
                                 name='name'
                                 value={name}
                                 onChange={onInputChange}
                             />
+                            <div className='invalid-feedback'>
+                                {nameValid}
+                            </div>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="floatingInput">Correo electrónico</label>
                             <input
-                                type="email"
-                                className="form-control form-field-contact"
+                                type="text"
+                                className={`form-control form-field-contact ${(!!emailValid && formSubmitted) ? 'is-invalid' : ''}`}
                                 placeholder="nombre@ejemplo.com"
                                 name='email'
                                 value={email}
                                 onChange={onInputChange}
                             />
+                            <div className='invalid-feedback'>
+                                {emailValid}
+                            </div>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="floatingTextarea2">Mensaje</label>
                             <textarea
                                 type="text"
-                                className="form-control form-field-contact"
+                                className={`form-control form-field-contact ${(!!messageValid && formSubmitted) ? 'is-invalid' : ''}`}
                                 placeholder="Introduzca el mensaje aquí"
                                 rows="5"
                                 name='message'
                                 value={message}
                                 onChange={onInputChange}
                             />
+                            <div className='invalid-feedback'>
+                                {messageValid}
+                            </div>
                         </div>
                         <div className='d-flex justify-content-center'>
                             <input
